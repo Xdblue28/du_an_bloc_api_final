bfebb133cbda6507462099b40ea582d022635de4
Hiep HX <hiephx@intern.vn>
2026-07-09 09:47:40 +0700

    Clean branch

--- lib/features/product/domain/entities/product_category.dart
+++
@@ -1,5 +0,0 @@
-class ProductCategory {
-  int id;
-  String name;
-  ProductCategory({required this.id, required this.name});
-}
--- lib/features/product/domain/repositories/product_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bai8_duan_final_bloc/features/product/domain/entities/product.dart';
-
-abstract class ProductRepository {
-  Future<List<Product>> loadProduct(int? categoryId);
-  Future<void> addProduct(Product product);
-  Future<void> deleteProduct(int id);
-  Future<void> updateProduct(Product product);
-}
